e94418b4edd76b85e61003ce0dfbed038d5e444b
yaswant raj <yaswantraj738@gmail.com>
2025-11-10 21:11:13 +0530

    Add files via upload

---
+++ electiricity bill.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int units;
+	float amount,charge,total_amount;
+	printf("Enter the number of units:");
+	scanf("%d",&units);
+	if(units<=50)
+	{
+		amount=units*2.50;
+		charge=30;
+	}
+	else if(units<=100)
+	{
+		amount=units*3.00;
+		charge=50;
+	}
+	else if(units<=200)
+	{
+		amount=units*3.50;
+		charge=75;
+	}
+	else if(units<=300)
+	{
+		amount=units*4.00;
+	}
+	else
+	{
+		amount=units*5.00;
+		charge=125;
+	}
+	total_amount=amount+charge;
+	printf("Electricity bill:%d\n",total_amount);
+	return 0;
+}
